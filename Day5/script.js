6780adf55337546176eefdade3254c4b0486b5a6
harshgarg2110 <harshgarg2110@gmail.com>
2024-10-14 10:30:44 +0530

    day5

---
+++ Day5/script.js
@@ -0,0 +1,74 @@
+// function display()
+// {
+//     // alert("Hello JS");
+//     console.log("hello js")
+// }
+// display();
+
+// let obj = {
+//     'name': 'Rishabh',
+//     id: 135,
+// }
+// console.log(typeof obj);
+// console.log(obj)
+
+// let arr =[12,21,34,3,1];
+// console.log(arr)
+// console.log(typeof arr)
+
+
+function validate()
+{
+   let x=document.getElementById("un").value;
+   let y=document.getElementById("pass").value;
+
+   if(x=="harsh" && y=="admin"){
+    document.getElementById('text').innerHTML="Login Success"
+
+   }
+   else
+      document.getElementById('text').innerHTML="Login Failed"
+}
+
+
+
+// let obj = {
+//     'name': 'harsh',
+//     id: 135,
+// }
+// console.log(typeof obj);
+// console.log(obj)
+
+// let arr =[12,21,34,3,1];
+// console.log(arr)
+// console.log(typeof arr)
+
+
+
+
+// let xx =Number("12");
+// let str=Number("123");
+// let b = boolean(0);
+// console.log(typeof xx);
+// console.log(typeof str);
+// console.log(b);
+// console.log(typeof b);
+
+
+
+// let a =12;
+// let c ='12';
+// console.log(a==c);
+// console.log(a===c);
+
+
+// let a=() =>{
+//    console.log("hello");
+// };
+
+// a();
+
+// let aa = (x,y) => {
+//     console.log(x+y);
+// }
+// aa(12,31);
